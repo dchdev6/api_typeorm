eb2e569a6cb8f1d37e930c9c6fe5431385b47f1f
unknown <YY105@ccs.local>
2025-03-05 01:48:46 -0800

    Added and Updated config.json and package.json and their depedencies

---
+++ server.js
@@ -0,0 +1,21 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('_middleware/error-handler');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// API Routes
+app.use('/users', require('./users/users.controller'));
+
+// Global Error Handler
+app.use(errorHandler);
+
+// Start the server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80 ) : 4000;
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+});
