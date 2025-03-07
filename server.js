44639771d2e276d706ad388759b8e71a39c078e2
Sean Joseph C. Arcana <chdev6@gmail.com>
2025-03-08 00:50:56 +0800

    Updated node and added sequelize in node_modules as it is not in instruction

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('rootpath')();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('_middleware/error-handler');
+const errorHandler = require('_middleware/error-handler.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,5 +17,5 @@ app.use(errorHandler);
 // Start the server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80 ) : 4000;
 app.listen(port, () => {
-    console.log('Server listening on port ' + port);
+    console.log('Server listening on port ${port}');
 });
